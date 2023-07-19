e46e58100e8c17ad22d48a5ae77fa36178121bae
Luis Liu <luis_liu@dell.com>
2023-07-19 10:47:14 +0800

    add dockerfile
    Signed-off-by: Luis Liu <luis_liu@dell.com>

---
+++ docker/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+
+set -ex
+
+ROOT_DIR=$(readlink -f $(dirname $0)/..)
+RUST_JOBS=${RUST_JOBS:-4}
+BASE_IMAGE=ubuntu:20.04
+
+# Make sure to always have fresh base image.
+docker pull ${BASE_IMAGE}
+
+# Build pravega-prod image which includes the binaries for all applications.
+DOCKER_BUILDKIT=1 docker build \
+    -t pravega/gstreamer:pravega-prod \
+    --build-arg RUST_JOBS=${RUST_JOBS} \
+    --build-arg BASE_IMAGE=${BASE_IMAGE} \
+    --target prod \
+    -f ${ROOT_DIR}/docker/Dockerfile \
+    ${ROOT_DIR}
